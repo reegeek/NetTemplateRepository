409517e9f2c7a704a44d1fc0e63548f0163fac4b
Reegeek <regis.fricker@gmail.com>
2020-04-12 14:55:30 +0200

    Add MathUtils file for test.

---
+++ src/Template.Tests/MinTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Template.Tests
+{
+    [TestFixture]
+    class PowerOf2Tests
+    {
+        [TestCase(2, true)]
+        [TestCase(3, false)]
+        [TestCase(4, true)]
+        [TestCase(1024, true)]
+        public void Should_power_of_2(int x, bool expected)
+        {
+            var result = MathUtils.PowerOf2(x);
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/Template/MathUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace Template
+{
+    public class MathUtils
+    {
+        public static bool PowerOf2(int v)
+        {
+            return (v & (v - 1)) == 0;
+        }
+    }
+}
