e2f963f18b4622939b2d947b6d66a9dfd22d1b3d
Reegeek <regis.fricker@gmail.com>
2020-04-12 15:50:00 +0200

    Add benchmark test.

---
+++ src/Template.Benchmark/PowerOf2.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Template.Benchmark
+{
+    public class PowerOf2
+    {
+        public const int n = 1000;
+        [Benchmark(OperationsPerInvoke = n)]
+        public bool Custom()
+        {
+            bool result = false;
+            for (int i = 0; i < n; i++)
+            {
+                result = MathUtils.PowerOf2(1024);
+            }
+            return result;
+        }
+    }
+}
