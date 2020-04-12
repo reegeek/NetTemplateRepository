4186ab3d75b31b66942746afc4c1a0ec4db8aca5
Reegeek <regis.fricker@gmail.com>
2020-04-12 11:54:02 +0200

    Add project skeleton.

---
+++ src/Template.Benchmark/Benchmark.Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace StructLinq.Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            Console.ReadLine();
+        }
+    }
+}
